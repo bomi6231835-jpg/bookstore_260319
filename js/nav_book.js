475a01c965d41592588831bb0f3eb41e5e47fe5d
bomi6231835-jpg <bomi6231835@gmail.com>
2026-03-24 18:59:12 +0900

    20260314_section4

--- js/nav_book.js
+++ js/nav_book.js
@@ -1,54 +1,67 @@
 function BookManager(tabs, books, data) {
-  this.tabs = tabs;
-  this.books = books;
-  this.data = data;
-}
+            this.tabs = tabs;
+            this.books = books;
+            this.data = data;
+        }
 
-BookManager.prototype.update = function (setName) {
-  const items = this.data[setName];
+        BookManager.prototype.update = function (setName) {
+            const items = this.data[setName];
 
-  this.books.forEach((book, i) => {
-    const imgtag = book.querySelector("img");
-    const title = book.querySelector(".title");
+            this.books.forEach((book, i) => {
+                const imgtag = book.querySelector("img");
+                const title = book.querySelector(".title");
 
-    imgtag.src = items[i].img;
-    title.textContent = items[i].title;
-  });
-};
+                imgtag.src = items[i].img;
+                title.textContent = items[i].title;
+            });
+        };
 
-BookManager.prototype.init = function () {
-  this.tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-      const setName = tab.dataset.set;
-      this.update(setName);
-    });
-  });
-};
+        BookManager.prototype.init = function () {
+            this.tabs.forEach(tab => {
+                tab.addEventListener("click", () => {
+                    const setName = tab.dataset.set;
+                    this.update(setName);
+                });
+            });
+        };
 
-// 실행
-const tabs = document.querySelectorAll("nav li");
-const books = document.querySelectorAll(".book");
+        // 실행
+        const tabs = document.querySelectorAll("nav li");
+        const books = document.querySelectorAll(".book");
 
-const data = {
-  set1: [
-    { img: "./img/bestseller/img10.png", title: "A Doctor in the House" },
-    { img: "./img/bestseller/img11.png" , title: "Design of the 20th Century"},
-    {img: "./img/bestseller/img12.png" , title:"New Galaxy"}
-  ],
-  set2: [
-    { img: "./img/bestseller/img01.png", title: "New Galaxy" },
-    { img: "./img/bestseller/img02.png", title: "Life in the Garden" },
-    {img:"./img/bestseller/img03.png" , title: 'The HouseMaid'}
-  ],
-  set3:[{img:'./img/bestseller/img04.png',title:'뭐라도 되겠죠'},
-  {img:"./img/bestseller/img05.png", title: '나를 위로하는 날'},
-  {img:'./img/bestseller/img06.png' , title:'빙과'}
+        const data = {
+            set1: [
+                { img: "./img/bestseller/img10.png", title: "A Doctor in the House" },
+                { img: "./img/bestseller/img12.png", title: "New Galaxy" },
+                { img: "./img/bestseller/img11.png", title: "Design of the 20th Century" },
+                { img: "./img/bestseller/img16.png", title: "Frankenstein" },
+                { img: "./img/bestseller/img18.png", title: "My Brilliant Life" }
+            ],
+            
+            set2: [
+                { img: "./img/bestseller/img01.png", title: "New Galaxy" },
+                { img: "./img/bestseller/img02.png", title: "Life in the Garden" },
+                { img: "./img/bestseller/img03.png", title: 'The House Maid' },
+                {img:"./img/bestseller/img13.png",title: "Let's new English Start!"},
+                {img: "./img/bestseller/img08.png", title: '초승달 고래의 여행'}
+            ],
+            set3: [{ img: './img/bestseller/img04.png', title: '뭐라도 되겠죠' },
+            { img: "./img/bestseller/img05.png", title: '나를 위로하는 날' },
+            { img: './img/bestseller/img15.png',title:'이처럼 사소한 것들'},
+             { img: './img/bestseller/img19.png',title:'두근두근 내 인생'},
+             { img: './img/bestseller/img14.png',title:'우연은 비켜가지 않는다'}
 
-  ]
-};
+            ],
+            set4: [{img:"./img/bestseller/img07.png" , title: '누군가 필요한 오늘'},
+                {img: "./img/bestseller/img08.png", title: '초승달 고래의 여행'},
+                {img: "./img/bestseller/img09.png", title: '고양이와 함께한 순간'},
+                { img: "./img/bestseller/img05.png", title: '나를 위로하는 날' },
+                { img: "./img/bestseller/img06.png", title: '빙과' },
+                ]
+            };
 
 
 
-const manager = new BookManager(tabs, books, data);
-console.log(manager)
-manager.init();
+        const manager = new BookManager(tabs, books, data);
+        console.log(manager)
+        manager.init();
--- js/swiper.js
+++ js/swiper.js
@@ -1,61 +1,16 @@
-function BookManager(tabs, books, data) {
-            this.tabs = tabs;
-            this.books = books;
-            this.data = data;
-        }
-
-        BookManager.prototype.update = function (setName) {
-            const items = this.data[setName];
-
-            this.books.forEach((book, i) => {
-                const imgtag = book.querySelector("img");
-                const title = book.querySelector(".title");
-
-                imgtag.src = items[i].img;
-                title.textContent = items[i].title;
-            });
-        };
-
-        BookManager.prototype.init = function () {
-            this.tabs.forEach(tab => {
-                tab.addEventListener("click", () => {
-                    const setName = tab.dataset.set;
-                    this.update(setName);
-                });
-            });
-        };
-
-        // 실행
-        const tabs = document.querySelectorAll("nav li");
-        const books = document.querySelectorAll(".book");
-
-        const data = {
-            set1: [
-                { img: "./img/bestseller/img10.png", title: "A Doctor in the House" },
-                { img: "./img/bestseller/img11.png", title: "Design of the 20th Century" },
-                { img: "./img/bestseller/img12.png", title: "New Galaxy" },
-                { img: "./img/bestseller/img01.png", title: "New Galaxy" },
-                { img: "./img/bestseller/img02.png", title: "Life in the Garden" }
-
-            ],
-            set2: [
-                { img: "./img/bestseller/img01.png", title: "New Galaxy" },
-                { img: "./img/bestseller/img02.png", title: "Life in the Garden" },
-                { img: "./img/bestseller/img03.png", title: 'The House Maid' }
-            ],
-            set3: [{ img: './img/bestseller/img04.png', title: '뭐라도 되겠죠' },
-            { img: "./img/bestseller/img05.png", title: '나를 위로하는 날' },
-            { img: './img/bestseller/img06.png',title:'빙과'} 
-            ],
-            set4: [{img:"./img/bestseller/img07.png" , title: '누군가 필요한 오늘'},
-                {img: "./img/bestseller/img08.png", title: '초승달 고래의 여행'},
-                {img: "./img/bestseller/img09.png", title: '고양이와 함께한 순간'}
-            ]
-            
-        };
-
-
-
-        const manager = new BookManager(tabs, books, data);
-        console.log(manager)
-        manager.init();
+// #slider swiper
+var slider_swiper = new Swiper(".sliderSwiper", {
+    navigation: {
+        nextEl: "#slider .swiper-button-next",
+        prevEl: "#slider .swiper-button-prev",
+    },
+    pagination: {
+        el: "#slider .swiper-pagination",
+        clickable: true,
+       
+    },
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
+});
